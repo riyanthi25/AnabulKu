8dc3aaee2ed661092b85d40dbba11455a9a14e2f
putu.riyanthi.ng@gmail.com <putu.riyanthi.ng@gmail.com>
2024-07-31 15:12:01 +0800

    Adopt page changes, breedInfo pagination

--- src/pages/adoptionPage.jsx
+++ src/pages/adoptionPage.jsx
@@ -1,9 +1,24 @@
-import Description from "../components/description";
+import { Center, Image } from "@mantine/core";
 
 export default function AdoptionPage() {
   return (
     <>
-      <Description text="THIS IS ADOPTION PAGE!" />
+      <div className="grid grid-cols-2 gap-4 bg-[url('/src/assets/bg-pattern.svg')] py-5 pr-20 ">
+        <div>
+          <Center>
+            <Image src="/src/assets/adoption.png" w={300} h={300}></Image>
+          </Center>
+        </div>
+        <div className="text-5xl pt-24">
+          <p>Find the Right </p>
+          <p>Cat for You</p>
+          <div className="text-5xl">
+            <p className="text-xl">
+              Tak hanya manusia yang butuh rumah, hewan pun perlu...
+            </p>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
--- src/pages/breedInfoPage.jsx
+++ src/pages/breedInfoPage.jsx
@@ -13,35 +13,64 @@ import {
   Center,
   Title,
   Pagination,
+  Box,
 } from "@mantine/core";
+import { usePagination } from "@mantine/hooks";
+export const loader = async () => {
+  try {
+    const cat_breeds = await fetch_cats_api(`/breeds`);
 
-export const loader = async (page = 1, limit = 16) => {
-  const cats = await fetch_cats_api(
-    "/images/search?has_breeds=true&limit=100&order=ASC"
-  );
-
-  // Filter out to only have one image per breed
-  const uniqueBreeds = [];
-  const uniqueCats = cats.filter((cat) => {
-    const breed = cat.breeds[0].id;
-    if (!uniqueBreeds.includes(breed)) {
-      uniqueBreeds.push(breed);
-      return true;
+    if (!Array.isArray(cat_breeds)) {
+      throw new Error("Invalid data format for cat breeds");
     }
-    return false;
-  });
 
-  // Return only 8 unique breed images
-  return uniqueCats.slice(0, 16);
+    const fetch_image = async (id) => {
+      try {
+        const response = await fetch_cats_api(`/images/${id}`);
+        if (response && response.url) {
+          return response.url;
+        }
+        throw new Error(`No URL found for image ID: ${id}`);
+      } catch (error) {
+        console.error("Image fetch error:", error);
+        return "";
+      }
+    };
+
+    const imagesUrlPromises = cat_breeds.map((cat) =>
+      fetch_image(cat.reference_image_id)
+    );
+    const imageUrls = await Promise.all(imagesUrlPromises);
+
+    return cat_breeds.map((cat, index) => ({
+      ...cat,
+      url: imageUrls[index],
+    }));
+  } catch (error) {
+    console.error("Loader error:", error);
+    return [];
+  }
 };
 
 export default function BreedInfoPage() {
   const cats = useLoaderData();
-  console.log(cats);
+  const catsPerPage = 8;
+  const [visibleCats, setVisibleCats] = useState(cats.slice(0, catsPerPage));
+  const totalCats = Math.ceil(cats.length / catsPerPage);
+  const pagination = usePagination({
+    total: totalCats,
+    initialPage: 1,
+    onChange(page) {
+      const start = (page - 1) * catsPerPage;
+      const end = start + catsPerPage;
+
+      setVisibleCats(cats.slice(start, end));
+    },
+  });
 
   return (
     <>
-      <div
+      <Box
         style={{
           backgroundColor: "#FF9EAA",
           color: "#2b0806",
@@ -55,55 +84,62 @@ export default function BreedInfoPage() {
         </Center>
         <Container size="xl" style={{ paddingTop: "30px" }}>
           <Grid gutter="xl">
-            {cats.map((cat) => (
-              <Grid.Col span={{ sm: 6, md: 4, lg: 3 }} key={cat.id}>
-                <Card shadow="sm" padding="lg" radius="lg">
-                  <Card.Section>
-                    <Image
-                      mah={{ sm: 300, md: 180 }}
-                      fit="cover"
-                      src={cat.url}
-                      alt={cat.breeds[0].name}
-                    />
-                  </Card.Section>
-                  <Text size="xl" mt={0} style={{ paddingTop: "15px" }}>
-                    {cat.breeds[0].name}
-                  </Text>
-                  <Text mt={0}>Origin From: {cat.breeds[0].origin}</Text>
-                  <Group
-                    justify="flex-start"
-                    mt="md"
-                    mb="xs"
-                    style={{ gap: "2px" }}
-                  >
-                    <Badge color="pink">
-                      Adaptation: {cat.breeds[0].adaptability}
-                    </Badge>
-                    <Badge color="pink">
-                      Love: {cat.breeds[0].affection_level}
-                    </Badge>
-                    <Badge color="pink">
-                      Social: {cat.breeds[0].social_needs}
-                    </Badge>
-                  </Group>
-                  <Button
-                    color="#3AA6B9"
-                    mt="md"
-                    radius="md"
-                    component={Link}
-                    to=""
-                  >
-                    Lihat Detail...
-                  </Button>
-                </Card>
-              </Grid.Col>
-            ))}
+            {visibleCats.length != 0 ? (
+              visibleCats.map((cat) => (
+                <Grid.Col span={{ sm: 6, md: 4, lg: 3 }} key={cat.id}>
+                  <Card shadow="sm" padding="lg" radius="lg">
+                    <Card.Section>
+                      <Image
+                        mah={{ sm: 300, md: 180 }}
+                        fit="cover"
+                        src={cat.url}
+                        alt={cat.name}
+                        fallbackSrc="https://placehold.co/600x400?text=couldn&#39;t%20load%20image"
+                      />
+                    </Card.Section>
+                    <Text size="xl" mt={0} style={{ paddingTop: "15px" }}>
+                      {cat.name}
+                    </Text>
+                    <Text mt={0}>Origin From: {cat.origin}</Text>
+                    <Group
+                      justify="flex-start"
+                      mt="md"
+                      mb="xs"
+                      style={{ gap: "2px" }}
+                    >
+                      <Badge color="pink">Adaptation: {cat.adaptability}</Badge>
+                      <Badge color="pink">Love: {cat.affection_level}</Badge>
+                      <Badge color="pink">Social: {cat.social_needs}</Badge>
+                    </Group>
+                    <Button
+                      color="#3AA6B9"
+                      mt="md"
+                      radius="md"
+                      component={Link}
+                      to=""
+                    >
+                      Lihat Detail...
+                    </Button>
+                  </Card>
+                </Grid.Col>
+              ))
+            ) : (
+              <Grid.Col span={12}>TIdak ada data</Grid.Col>
+            )}
           </Grid>
         </Container>
         <Center>
-          <Pagination total={10} style={{ paddingTop: "50px" }} />
+          <Box mt={"xl"}>
+            <Pagination
+              total={totalCats}
+              withEdges
+              size={"lg"}
+              onChange={(range) => pagination.setPage(range)}
+              onFirstPage={() => pagination.setPage(1)}
+            />
+          </Box>
         </Center>
-      </div>
+      </Box>
     </>
   );
 }
