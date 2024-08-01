775a7120b20d9186fbfc32246187dbc745843409
putu.riyanthi.ng@gmail.com <putu.riyanthi.ng@gmail.com>
2024-08-01 10:03:03 +0800

    New footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,96 +1,5 @@
-// import React from "react";
-// import { Link } from "react-router-dom";
-// import { Container, Grid, Text, Image } from "@mantine/core";
-
-// const currentYear = new Date().getFullYear();
-
-// const links = [
-//   { to: "/", label: "Home" },
-//   { to: "/adopt", label: "Adopt" },
-//   { to: "/breed", label: "Breed" },
-//   { to: "/quiz", label: "Quiz" },
-//   // add new link here!
-// ];
-
-// const socials = [
-//   {
-//     to: "https://www.instagram.com/riyanthi_ng",
-//     label: "Instagram",
-//     icon: "/src/assets/instagram.svg",
-//   },
-//   {
-//     to: "https://wa.me/085737005452",
-//     label: "WhatsApp",
-//     icon: "/src/assets/whatsapp.svg",
-//   },
-//   {
-//     to: "https://linkedin.com/in/putu-yoeliessya-riyanthi-b8264a280",
-//     label: "LinkedIn",
-//     icon: "/src/assets/linkedin.svg",
-//   },
-// ];
-
-// export default function Footer() {
-//   return (
-//     <footer
-//       style={{
-//         backgroundColor: "#FFD0D0",
-//         padding: "70px 0px 50px 0px",
-//       }}
-//     >
-//       <Container size="xl">
-//         <Grid>
-//           <Grid.Col span={6}>
-//             <nav
-//               style={{
-//                 display: "flex",
-//                 justifyContent: "center",
-//                 paddingBottom: "10px",
-//                 gap: "2rem",
-//               }}
-//             >
-//               {links.map((link) => (
-//                 <Link
-//                   key={link.to}
-//                   to={link.to}
-//                   style={{
-//                     color: "black",
-//                     textDecoration: "none",
-//                   }}
-//                 >
-//                   {link.label}
-//                 </Link>
-//               ))}
-//             </nav>
-//             <Text align="center" size="sm" color="black">
-//               &copy; {currentYear} AnabulKu. All rights reserved.
-//             </Text>
-//           </Grid.Col>
-//           <Grid.Col span={6}>
-//             {socials.map((social) => (
-//               <Link
-//                 target="blank"
-//                 key={social.to}
-//                 to={social.to}
-//                 style={{
-//                   paddingRight: "60px",
-//                   display: "inline-block",
-//                   color: "whitesmoke",
-//                 }}
-//               >
-//                 <Image src={social.icon} w={30} h={30} />
-//               </Link>
-//             ))}
-//           </Grid.Col>
-//         </Grid>
-//       </Container>
-//     </footer>
-//   );
-// }
-
 import React from "react";
 import { Link } from "react-router-dom";
-import { Container, Grid, Text, Image } from "@mantine/core";
 
 const currentYear = new Date().getFullYear();
 
@@ -118,6 +27,16 @@ const socials = [
     label: "LinkedIn",
     icon: "/src/assets/linkedin.svg",
   },
+  {
+    to: "https://github.com/riyanthi25",
+    label: "Github",
+    icon: "/src/assets/github.svg",
+  },
+  {
+    to: "https://discordapp.com/users/873507001404112926",
+    label: "Discord",
+    icon: "/src/assets/discord.svg",
+  },
 ];
 
 export default function Footer() {
@@ -126,6 +45,7 @@ export default function Footer() {
       className="md:pt-0 pt-2 md:pb-4 pb-3"
       style={{
         backgroundColor: "#FFD0D0",
+        color: "#2b0806",
       }}
     >
       <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
@@ -139,7 +59,7 @@ export default function Footer() {
               className="h-10 md:mb-3"
               alt="anabulku logo"
             />
-            <span className="self-center text-2xl font-semibold whitespace-nowrap">
+            <span className="self-center text-[1.5rem] font-semibold whitespace-nowrap">
               AnabulKu
             </span>
           </a>
@@ -154,13 +74,27 @@ export default function Footer() {
           </ul>
         </div>
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-        <span className="block text-base sm:text-center">
-          © {currentYear}{" "}
-          <a href="https://flowbite.com/" className="hover:underline">
-            AnabulKu™
-          </a>
-          . All Rights Reserved.
-        </span>
+        <div className="sm:flex sm:items-center sm:justify-between">
+          <span className="text-sm sm:text-center ">
+            © {currentYear}{" "}
+            <a href="/" class="hover:underline">
+              AnabulKu
+            </a>
+            . All Rights Reserved.
+          </span>
+          <div className="flex mt-4 sm:justify-center sm:mt-0 md:mr-6">
+            {socials.map((social) => (
+              <a
+                key={social.label}
+                href={social.to}
+                className="ms-5 "
+              >
+                <img src={social.icon} alt={social.label} className="md:w-6 w-5 md:h-6" />
+                <span className="sr-only ">{social.label}</span>
+              </a>
+            ))}
+          </div>
+        </div>
       </div>
     </footer>
   );
