e35cd19aa5725df54063ae7ab975d819884ef9b4
putu.riyanthi.ng@gmail.com <putu.riyanthi.ng@gmail.com>
2024-08-01 09:22:48 +0800

    Fixing breed bug

--- src/layout/main.jsx
+++ src/layout/main.jsx
@@ -5,6 +5,7 @@ import Footer from "../components/footer";
 import Header from "../components/header";
 import ScrollToTop from "../components/scrollToTop";
 import { useDisclosure } from "@mantine/hooks";
+import { Box } from "@mantine/core";
 
 export default function LayoutMain() {
   const [opened, { toggle }] = useDisclosure(false);
